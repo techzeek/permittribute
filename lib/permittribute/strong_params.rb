ab7cd554dc1e96ed2bd65742c909de02bb4fe4ff
Priyank Gupta <priyank.gupta@vinsol.com>
2015-07-27 15:09:45 +0530

    remove unwanted code

--- lib/permittribute/strong_params.rb
+++ lib/permittribute/strong_params.rb
@@ -3,8 +3,6 @@ require 'permittribute/attrs'
 module Permittribute
   module StrongParams
     def permittribute
-      p "_"*80
-      p Permittribute::Attrs.authors
       Permittribute::Attrs
     end
   end
